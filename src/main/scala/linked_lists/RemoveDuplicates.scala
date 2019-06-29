d67d35d382c327c60250d044bcdca1a4c8c54cf3
amitgupta1202 <amitgupta1202@gmail.com>
2019-06-29 11:01:20 +0100

    linked list remove dups without modifying the original

--- src/main/scala/linked_lists/RemoveDuplicates.scala
+++ src/main/scala/linked_lists/RemoveDuplicates.scala
@@ -1,6 +1,7 @@
 package linked_lists
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 /*
   Write code to remove duplicates from an unsorted linked list
@@ -24,10 +25,27 @@ object RemoveDuplicates extends App {
       }
     }
 
+  def dd_without_modifying(linkedList: Option[LinkedList], lastNodeOfResult: LinkedList, values: mutable.Set[Int]): LinkedList = {
+    if (linkedList.isEmpty) lastNodeOfResult
+    else {
+      if (values(linkedList.get.value)) dd_without_modifying(linkedList.get.next, lastNodeOfResult, values)
+      else {
+        val newNode = LinkedList(linkedList.get.value)
+        lastNodeOfResult.next = Some(newNode)
+        dd_without_modifying(linkedList.get.next, newNode, values + linkedList.get.value)
+      }
+    }
+  }
+
   //Test case
   val ll = LinkedList(1, LinkedList(2, LinkedList(3, LinkedList(3, LinkedList(3, LinkedList(4))))))
 
   assert(prettyString(ll) == "1 --> 2 --> 3 --> 3 --> 3 --> 4")
+
+  val result = LinkedList(ll.value)
+  dd_without_modifying(ll.next, result, mutable.Set.empty)
+  assert(prettyString(result) == "1 --> 2 --> 3 --> 4")
+
   dd(ll)
   assert(prettyString(ll) == "1 --> 2 --> 3 --> 4")
 }
