f01f19ec02e08a39d3e5c536dd07175ca67b8161
amitgupta1202 <amitgupta1202@gmail.com>
2019-06-30 23:21:51 +0100

    multiply

--- src/main/scala/dynamic_programming/Multiply.scala
+++ src/main/scala/dynamic_programming/Multiply.scala
@@ -5,7 +5,7 @@ object Multiply extends App {
   def multiply(num1: Int, num2: Int): Int = {
     val (smaller, bigger) = if (num1 < num2) (num1, num2) else (num2, num1)
 
-    def helper(s: Int, b: Int): Int = {
+    def helper(s: Int, b: Int): Int =
       if (s == 0) 0
       else if (s == 1) b
       else {
@@ -13,7 +13,6 @@ object Multiply extends App {
         if (s % 2 == 0) temp << 1 //or temp + temp
         else (temp << 1) + b
       }
-    }
 
     helper(smaller, bigger)
   }
